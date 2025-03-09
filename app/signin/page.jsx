5a27b1bb4c3fcc6b8101be1de7450fbc227bbb87
KaraIT123 <coulibalykaramoko@outlook.com>
2025-03-09 06:16:07 -0400

    responsive login

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -37,10 +37,12 @@ const SignIn = () => {
 
     const [index, setIndex] = useState(1)
     const increment = () => {
-        let i = index == testimonials.length ? 0 : index + 1;
-        console.log(i);
-        console.log(testimonials.length);
+        let i = index == testimonials.length - 1 ? 0 : index + 1;
+        setIndex(i);
+    }
 
+    const decrement = () => {
+        let i = index == 0 ? testimonials.length - 1 : index - 1;
         setIndex(i);
     }
 
@@ -49,8 +51,8 @@ const SignIn = () => {
 
     return (
         <div className='container text-gray-500 py-20' id='top'>
-            <div className='flex p-20' >
-                <div className='flex flex-col gap-8 w-full p-20'>
+            <div className='flex lg:p-10 2xl:p-20' >
+                <div className='flex flex-col gap-8 w-full md:mx-16 lg:mx-0 lg:p-10 2xl:p-20'>
                     <div>
                         <p className='font-semibold text-5xl text-black'>Welcome</p>
                         <p className='text-xs'>Please enter your account details</p>
@@ -89,12 +91,12 @@ const SignIn = () => {
 
 
                 </div>
-                <div className='bg-primary relative w-full text-white rounded-2xl px-14 py-20'>
+                <div className='hidden xl:block bg-primary relative w-full text-white rounded-2xl px-14 py-20'>
                     <div className="bg-app-background rounded-bl-2xl absolute h-16 w-20 top-0 right-0 after:bg-primary after:content-[' '] after:rounded-tr-2xl after:w-8 after:h-8 after:absolute after:-left-8 after:shadow-app-background after:shadow-[13px_0_0]
                     before:bg-primary before:content-[' '] before:rounded-tr-2xl before:w-8 before:h-8 before:absolute before:-bottom-8 before:right-0 before:shadow-app-background before:shadow-[13px_0_0]"> </div>
 
                     <div className='flex flex-col gap-8'>
-                        <p className='text-6xl'>What's our students saids.</p>
+                        <p className='xl:text-5xl 2xl:text-6xl'>What's our students saids.</p>
 
                         <Quote size={25} />
 
@@ -106,7 +108,7 @@ const SignIn = () => {
                         </div>
 
                         <div className='flex gap-2'>
-                            <button onClick={() => setIndex(index - 1)} className='p-4 border-[.5px] border-app-background rounded-md hover:text-black hover:bg-app-background cursor-pointer'>
+                            <button onClick={() => decrement()} className='p-4 border-[.5px] border-app-background rounded-md hover:text-black hover:bg-app-background cursor-pointer'>
                                 <ArrowLeft />
                             </button>
                             <button onClick={() => increment()} className='p-4 bg-app-background text-black rounded-md hover:border border-app-background hover:bg-primary hover:text-white cursor-pointer'>
